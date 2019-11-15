bd5f3f11cc1286d43a1a2584c72dc01ec614a40e
lighteagle <sofyan.agus18@gmail.com>
2019-11-15 15:59:38 +0700

    Updates

---
+++ precache-manifest.7d86b717758476440f0f2a232356c94d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466bca7231ea6dc1b12109c1bd26fd83",
+    "url": "/learn-react-2/index.html"
+  },
+  {
+    "revision": "14edb8e965ad6886ee6f",
+    "url": "/learn-react-2/static/css/main.baea2f77.chunk.css"
+  },
+  {
+    "revision": "ca5c7c625535bba746a8",
+    "url": "/learn-react-2/static/js/2.1744c23a.chunk.js"
+  },
+  {
+    "revision": "14edb8e965ad6886ee6f",
+    "url": "/learn-react-2/static/js/main.cdc168d3.chunk.js"
+  },
+  {
+    "revision": "279fbfadf9bb90b1d474",
+    "url": "/learn-react-2/static/js/runtime-main.a10e9e9b.js"
+  }
+]);
